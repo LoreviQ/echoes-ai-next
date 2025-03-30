5573e51f57ce2c365996d8af933b527def156656
LoreviQ <lorevi@protonmail.com>
2025-03-30 17:55:33 +0100

    Added basic routes

---
+++ src/app/home/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function HomePage() {
+    return (
+        <div>
+            <h1>Home Page</h1>
+            <p>This is the main content area for the home page.</p>
+        </div>
+    );
+}
---
+++ src/app/notifications/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function NotificationsPage() {
+    return (
+        <div>
+            <h1>Notifications Page</h1>
+            <p>This is where notifications will be displayed.</p>
+        </div>
+    );
+}
