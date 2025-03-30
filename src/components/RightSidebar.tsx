a85994c41dd588c24eeb19c4e8d70dd4d52e1274
LoreviQ <lorevi@protonmail.com>
2025-03-30 22:02:01 +0100

    Added search bar

--- src/components/RightSidebar.tsx
+++ src/components/RightSidebar.tsx
@@ -1,17 +1,25 @@
 import React from "react";
+import { SearchIcon } from "@/assets/icons";
 
 export default function RightSidebar() {
     return (
-        <div className="p-4 bg-black text-white w-[340px] xl:w-[400px] h-screen transition-all duration-300 border-l border-gray-600">
-            <h2 className="text-xl font-bold mb-4">
-                <span className="xl:hidden">RS</span>
-                <span className="hidden xl:inline">Right Sidebar</span>
-            </h2>
-            <div className="space-y-2">
-                <p>Recent activity</p>
-                <p>Notifications</p>
-                <p>User settings</p>
+        <div className="pt-4 pl-10 bg-black text-white w-340px h-screen transition-all duration-300 border-l border-gray-600">
+            <div className="relative">
+                <Search />
             </div>
+        </div>
+    );
+}
+
+function Search() {
+    return (
+        <div className="relative group">
+            <SearchIcon className="absolute left-3 top-1/2 transform -translate-y-1/2 w-5 h-5 text-gray-400 transition-colors duration-200 group-focus-within:text-white" />
+            <input
+                type="text"
+                placeholder="Search..."
+                className="w-full bg-black border border-gray-600 rounded-xl py-2 pl-10 pr-4 text-white placeholder-gray-400 focus:border-white focus:outline-none transition-colors duration-200"
+            />
         </div>
     );
 }
