ca4495c809d741d05b3ef7655f1f8e6d267701ce
LoreviQ <lorevi@protonmail.com>
2025-03-31 18:27:50 +0100

    Made monorepo split repo

--- backend/src/routes/characters.ts
+++
@@ -1,9 +0,0 @@
-import express, { Router } from 'express';
-import { createPost } from '../controllers/posts';
-
-const router: Router = express.Router();
-
-// POST /characters/:character_id/posts - Create a new post for a character
-router.post('/:character_id/posts', createPost);
-
-export default router; 
