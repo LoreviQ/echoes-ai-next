8bd3f226c35fa440b1b9c91978f3227ed14f5688
LoreviQ <lorevi@protonmail.com>
2025-03-31 13:09:18 +0100

    Made sidebar scrollable

--- src/components/LeftSidebar.tsx
+++ src/components/LeftSidebar.tsx
@@ -25,29 +25,31 @@ const navigationItems = [
 export default function LeftSidebar() {
     return (
         <div className={`bg-black text-white w-[84px] xl:w-[280px] h-screen transition-all duration-300 border-r border-gray-600 flex flex-col`}>
-            <div className="p-4 space-y-4">
-                <TypefaceOutlined
-                    text="EchoesAI"
-                    path="/"
-                    outlineColour="white"
-                    className="hidden xl:block text-5xl"
-                />
-                <TypefaceOutlined
-                    text="EAI"
-                    path="/"
-                    outlineColour="white"
-                    className="xl:hidden text-3xl"
-                />
-                <div className="flex flex-col space-y-2">
-                    {navigationItems.map((item) => (
-                        <div key={item.path} className="w-full">
-                            <NavButton
-                                label={item.label}
-                                path={item.path}
-                                icon={item.icon}
-                            />
-                        </div>
-                    ))}
+            <div className="p-4 flex-1 overflow-y-auto">
+                <div className="space-y-4">
+                    <TypefaceOutlined
+                        text="EchoesAI"
+                        path="/"
+                        outlineColour="white"
+                        className="hidden xl:block text-5xl"
+                    />
+                    <TypefaceOutlined
+                        text="EAI"
+                        path="/"
+                        outlineColour="white"
+                        className="xl:hidden text-3xl"
+                    />
+                    <div className="flex flex-col space-y-2">
+                        {navigationItems.map((item) => (
+                            <div key={item.path} className="w-full">
+                                <NavButton
+                                    label={item.label}
+                                    path={item.path}
+                                    icon={item.icon}
+                                />
+                            </div>
+                        ))}
+                    </div>
                 </div>
             </div>
             <UserSection />
