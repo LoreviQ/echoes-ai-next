a0f1a749a213a9625117e99c14fb6609d06f985f
LoreviQ <lorevi@protonmail.com>
2025-04-03 00:12:43 +0100

    Added spascer so top message is visible

--- src/components/content/Messages.tsx
+++ src/components/content/Messages.tsx
@@ -155,7 +155,7 @@ function ChatWindow({ messages, isLoading, onSendMessage, isSending }: ChatWindo
     return (
         <div className="flex flex-col flex-1 h-full">
             {/* Messages container */}
-            <div className="flex-1 overflow-y-auto p-4 flex flex-col-reverse">
+            <div className="flex-1 overflow-y-auto p-4 pt-24 flex flex-col-reverse">
                 <div>
                     {messages.length === 0 ? (
                         <div className="flex justify-center items-center text-zinc-500">
